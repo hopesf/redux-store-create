89d11ad4aa439cba69fbc41e6de82a6f1be93291
hopesf <gecin18@gmail.com>
2019-09-17 22:59:22 +0300

    mantigi anlamak

--- src/index.js
+++ src/index.js
@@ -3,14 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import {createStore} from "redux";
+import {createStore} from 'redux';
 
-function reducer() {
-    return('test reducer fonksiyonu');
+function reducer(state, action) {
+    if(action.type === 'ChangeState'){
+        return action.payload.newState
+    }
+    else{
+        return 'State';
+    }
 }
+const Store = createStore(reducer);
+console.log(Store.getState());
 
-const store = createStore(reducer);
-console.log(store.getState());
+const action ={
+    type:'ChangeState',
+    payload:{
+        newState:'yeni state'
+    }
+};
+
+const action2 ={
+    type:'ChangeState',
+    payload:{
+        newState:'state 2'
+    }
+};
+
+Store.subscribe(()=>{
+    console.log('store updated:', Store.getState());
+});
+
+Store.dispatch(action);
+Store.dispatch(action2);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
